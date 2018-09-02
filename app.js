849a2ac3fcf6c66f2b73dba68a5e199693508142
Mubarak Aminu <mubarakaminu340@gmail.com>
2018-09-02 14:25:59 +0100

    single get update post and delete

--- app.js
+++ app.js
@@ -1,10 +1,63 @@
 const http = new easyhttp();
 
 //get post
-http.get('https://jsonplaceholder.typicode.com/posts1', function(err, posts) {
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
   if (err) {
     console.log(err);
   } else {
     console.log(posts);
+  }
+});
+
+//Get sigle post
+
+http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, post) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+//Create Data
+const data = {
+  title: 'Custom post',
+  user_id: 2,
+  body: 'this is a custom post'
+};
+//POST request
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+//Updating post
+
+http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(
+  err,
+  post
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(post);
+  }
+});
+
+//Delete a post
+http.delete('https://jsonplaceholder.typicode.com/posts/5', function(
+  err,
+  response
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
   }
 });
